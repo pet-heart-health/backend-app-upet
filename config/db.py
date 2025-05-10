72fff57977e26c87f257013e8452b67c8d055a90
Diego Flores <u20221b394@upc.edu.pe>
2025-05-10 15:22:56 -0500

    configure for deploy

--- config/db.py
+++ config/db.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
+import os
 
 MYSQL_USER = 'root'
 MYSQL_PASSWORD = 'password'
@@ -10,7 +11,8 @@ MYSQL_PORT = '3306'
 MYSQL_DATABASE = 'defaultdb'
 
 #mysqlsh --sql --host=sfo1.clusters.zeabur.com --port=30777 --user=root --password=Y9UEFrxH14OgZ872K6TueyJjD53mts0Q --schema=zeabur
-URL_DATABASE = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}'
+#URL_DATABASE = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}'
+URL_DATABASE = os.getenv('DATABASE_URL', "mysql://root:AdZLWyqhVAchoUSCRRdtJrrocuzbMdVh@crossover.proxy.rlwy.net:56860/railway")
 
 engine = create_engine(URL_DATABASE)
 
