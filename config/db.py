34029ae7f3a0c343d75e67a0b3819ae29bd07d7c
Diego Flores <u20221b394@upc.edu.pe>
2025-05-10 20:48:49 -0500

    feat(deploy): added configurations

--- config/db.py
+++ config/db.py
@@ -12,7 +12,7 @@ MYSQL_DATABASE = 'defaultdb'
 
 #mysqlsh --sql --host=sfo1.clusters.zeabur.com --port=30777 --user=root --password=Y9UEFrxH14OgZ872K6TueyJjD53mts0Q --schema=zeabur
 #URL_DATABASE = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}'
-URL_DATABASE = os.getenv('DATABASE_URL', "mysql://root:AdZLWyqhVAchoUSCRRdtJrrocuzbMdVh@crossover.proxy.rlwy.net:56860/railway")
+URL_DATABASE = os.getenv('URL_DATABASE', "mysql+pymysql://root:AdZLWyqhVAchoUSCRRdtJrrocuzbMdVh@crossover.proxy.rlwy.net:56860/railway")
 
 engine = create_engine(URL_DATABASE)
 
