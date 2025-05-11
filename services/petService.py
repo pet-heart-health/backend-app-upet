dd61bf3a597a12ecb3ccccb0a468db3f8c896659
Marcelo rentería <u202216558@upc.edu.pe>
2025-05-10 23:18:55 -0500

    fix: Attemp at fixing Pet Creation Issue.

--- services/petService.py
+++ services/petService.py
@@ -15,36 +15,36 @@ from services.medical_history import MedicalHistoryService
 class PetServices:
     @staticmethod
     def create_new_pet(petowner_id: int, pet: PetSchemaPost, db: Session ):
-        pet_owner = db.query(PetOwner).filter(PetOwner.userId == petowner_id).first()
+        pet_owner = db.query(PetOwner).filter(PetOwner.id == petowner_id).first()
         if not pet_owner:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="El propietario de la mascota no existe.")
 
         if pet.weight <= 0:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El peso debe ser mayor a 0.")
 
- 
-        new_pet = Pet(petOwnerId=petowner_id, 
-                      name=pet.name, 
-                      birthdate=pet.birthdate, 
-                      weight=pet.weight, 
+
+        new_pet = Pet(petOwnerId=petowner_id,
+                      name=pet.name,
+                      birthdate=pet.birthdate,
+                      weight=pet.weight,
                       species=pet.species,
-                        breed=pet.breed, 
+                        breed=pet.breed,
                         gender= pet.gender,
                         image_url= pet.image_url)
         db.add(new_pet)
         db.commit()
         db.refresh(new_pet)  # Para cargar el ID generado
-        
-    
+
+
         medicalHistory = MedicalHistorySchemaPost(
-            petId=new_pet.id,  
+            petId=new_pet.id,
             date=datetime.date.today(),
             description=f"Creación de historial médico de {new_pet.name}"
         )
         MedicalHistoryService.add_medical_history(medicalHistory,db=db)
         return new_pet
 
-    
+
     @staticmethod
     def get_pet_by_user_id(pet_id: int, db: Session):
         pet = db.query(Pet).filter(Pet.id == pet_id).first()
@@ -54,24 +54,24 @@ class PetServices:
     def get_pets_by_petOwnerid(petOwner_id: int, db: Session):
         pets = db.query(Pet).filter(Pet.petOwnerId == petOwner_id).all()
         return pets
-    
+
     @staticmethod
     def update_pet(pet_id: int, pet: PetSchemaPost, db: Session):
         pet_db = db.query(Pet).filter(Pet.id == pet_id).first()
         if not pet_db:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Mascota no encontrada")
-  
+
         PetSchemaResponse.update_pet_from_schema(pet_db, pet)
         db.commit()
         return pet_db
-    
+
     @staticmethod
     def get_pet_by_id(pet_id: int, db: Session):
         pet = db.query(Pet).filter(Pet.id == pet_id).first()
         if not pet:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Pet not found")
         return pet
-    
+
     @staticmethod
     def delete_pet(pet_id: int, db: Session):
         pet = db.query(Pet).filter(Pet.id == pet_id).first()
@@ -80,7 +80,7 @@ class PetServices:
         db.delete(pet)
         db.commit()
         return pet
-       
+
     @staticmethod
     def get_petowners(db: Session):
         pet = db.query(Pet)
