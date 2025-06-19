acb90bd4c6a1f8bd646cb786c7eafa1feae9f019
Marcelo rentería <u202216558@upc.edu.pe>
2025-06-19 12:45:40 -0500

    fix: Db fix, for real this time.

--- config/db.py
+++ config/db.py
@@ -9,10 +9,10 @@ MYSQL_PASSWORD = 'root'
 MYSQL_HOST = 'localhost'
 MYSQL_PORT = '3306'
 MYSQL_DATABASE = 'defaultdb'
-
+86eec8a5-49dc-4c70-b770-b516d44ea5a4
 #mysqlsh --sql --host=sfo1.clusters.zeabur.com --port=30777 --user=root --password=Y9UEFrxH14OgZ872K6TueyJjD53mts0Q --schema=zeabur
 #URL_DATABASE = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}'
-URL_DATABASE = os.getenv('URL_DATABASE', "mysql+pymysql://root:AdZLWyqhVAchoUSCRRdtJrrocuzbMdVh@crossover.proxy.rlwy.net:56860/railway")
+URL_DATABASE = os.getenv('URL_DATABASE', "mysql+pymysql://root:MlIxTQyYilgIHJmNJwdztRcXGeFDxUZc@centerbeam.proxy.rlwy.net:49253/railway")
 
 engine = create_engine(URL_DATABASE)
 
