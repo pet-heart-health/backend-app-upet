1e9d03591a2f0e1d2dd4e26d8474797326f77971
Marcelo rentería <u202216558@upc.edu.pe>
2025-07-08 14:25:23 -0500

    fix: Attempt at time mismatch.

--- services/reminderService.py
+++ services/reminderService.py
@@ -33,7 +33,7 @@ class ReminderService:
         scheduler.add_job(
             NotificationService.send_reminder_notification,
             args=[new_reminder.id],
-            trigger=DateTrigger(run_date=new_reminder.date_time.astimezone(utc_tz))
+            trigger=DateTrigger(run_date=utc_tz(new_reminder.date_time)
 )
 
         return new_reminder
